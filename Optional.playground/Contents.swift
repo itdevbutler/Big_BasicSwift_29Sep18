cfab79d74cca2cb696d620a701818a21c145a741
itdev <it.dev@digitalbutler.co.th>
2018-09-29 14:34:32 +0700

    Optional Function on IOS

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+var numberInt:Int?
+
+print("numberInt ==> \(numberInt)")
+
+var numberString:String = "Five"
+
+//let number2Int:Int = Int(numberString)!
+//
+//print("number2Int ==> \(number2Int)")
+
+if var myNumber = Int(numberString) {
+    print("myNumber ==> \(myNumber)")
+}
+
