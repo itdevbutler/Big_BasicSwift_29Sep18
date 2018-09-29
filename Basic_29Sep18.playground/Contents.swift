7b82c5cabb132d3bb96d8720cb75461d6227b036
itdev <it.dev@digitalbutler.co.th>
2018-09-29 09:33:33 +0700

    variable and constant Success

---
+++ Basic_29Sep18.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//การประกาศตัวแปร และค่าคงที่
+//Implicit
+var nameString = "Doramon"
+var number1Int = 123
+var number3Dou = 12.34
+var status1Bol = true
+
+number1Int = 555
+
+
+
+//Explicit
+var surnameString: String = "Japane"
+var number2Int: Int = 123
+var number4Dou: Double = 12.34
+var status2Bol:Bool = false
+
+
+
+
+
+
+
+
+
